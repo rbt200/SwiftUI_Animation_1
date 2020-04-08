557370226ac9afe7bdff5c8218b4df94a6fd8831
rbt200 <myesoap@mail.ru>
2020-04-08 11:42:36 -0700

    Finish task

--- SwiftUI_Animation_1/ContentView.swift
+++ SwiftUI_Animation_1/ContentView.swift
@@ -14,16 +14,48 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            HStack {
+                Button("Hello, World!") {
+                }
+                .frame(width: 400, height: 60, alignment: .center)
+            }
+            .scaleEffect(self.isPressed ? 2 : 1)
+            .background(Color.green)
+            .cornerRadius(10)
+            .offset(y: self.isPressed ? -200 : -450)
+            .animation(.interpolatingSpring(mass: 2, stiffness: 100, damping: 10, initialVelocity: 0))
+            
             Button("Press Me"){
                 self.isPressed.toggle()
             }
-            if isPressed {
-                Text("Green")
-                    .background(Color.green)
-            } else {
-                Text("Red")
-                    .background(Color.red)
+            HStack {
+                Text(self.isPressed ? "UP" : "DOWN")
+                Image(systemName: "chevron.up.square")
+                    .rotationEffect(.degrees(self.isPressed ? 0 : 180))
+                    .animation(.default)
             }
+            
+            HStack {
+                Text("🌝")
+                    .font(.custom("Arial", size: 100))
+                    .offset(x: self.isPressed ?  UIScreen.main.bounds.width - 120 : 0 )
+//                    .rotationEffect(.degrees(self.isPressed ? 0 : 90))
+                    .animation(.interpolatingSpring(mass: 5, stiffness: 100, damping: 10, initialVelocity: 0))
+                
+                
+                Spacer()
+            }
+//            HStack {
+//                if isPressed {
+//                    Text("Green")
+//                        .background(Color.green)
+//                    Image(systemName: "chevron.up.square")
+//                } else {
+//                    Text("Red")
+//                        .background(Color.red)
+//                    Image(systemName: "chevron.down.square")
+//                }
+//            }
         }
     }
 }
